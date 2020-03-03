1bd0ce4e9589511af2adc75058ef9a64ad72c85e
Abel <abelmasila@gmail.com>
2020-03-03 17:00:51 +0300

    removed my api key

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 username = "sandbox"
-api_key = "2f8f7f90b4f98aba5ea51dbc94e5bb3715126ccde69e745379b8f48b9de0decc"
+api_key = ""
 africastalking.initialize(username, api_key)
 sms = africastalking.SMS
 
